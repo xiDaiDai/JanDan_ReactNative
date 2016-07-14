5b4a0c9d44606acc49da972dfe91d7f83c23a7e2
xiDaiDai <xishiwei@sina.cn>
2016-07-14 12:30:23 +0800

    upgrade

--- NewsList.android.js
+++ NewsList.android.js
@@ -4,7 +4,9 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import {
   AppRegistry,
   StyleSheet,
@@ -25,25 +27,25 @@ import LoadingMoreView from './LoadingMoreView';
 const url = "http://jandan.net/?oxwlxojflwblxbsapi=get_recent_posts&include=url,date,title,custom_fields&custom_fields=thumb_c,views&dev=1&page=";
 
 let pageIndex = 1;
- 
+
 class NewsList extends Component {
   constructor(props) {
     super(props);
-  
+
     this.state = {
-        isRefreshing:this.props.needRefresh,
-        dataSource:new ListView.DataSource({
-          rowHasChanged:(row1,row2)=>row1!==row2,
-        }),  
-        loaded:false,
-        loadMore: false,
-        newContent:null,
+      isRefreshing: this.props.needRefresh,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+      loadMore: false,
+      newContent: null,
     };
   }
 
-  componentDidMount(){
-     
-   
+  componentDidMount() {
+
+
     // this.setState({dataSource:this.state.dataSource.cloneWithRows(news.posts)});
     this.fetchNewsData();
   }
@@ -67,124 +69,135 @@ class NewsList extends Component {
     );
   }
 
- 
-  renderFooter(){
-    return(this.state.loadmore?<LoadingMoreView/>:null);
-    
+
+  renderFooter() {
+    return (this.state.loadmore ? <LoadingMoreView/> : null);
+
   }
 
 
-  renderNewsItem(newsItem){
-    return(
-      <TouchableHighlight 
+  renderNewsItem(newsItem) {
+    return (
+      <TouchableHighlight
             underlayColor='white'
             onPress={() => this.pressRow(newsItem.id,newsItem.title)} >
         <View style={{backgroundColor:'white',flexDirection:'column'}}>
-          <View style={{justifyContent:'center', flexDirection :'row',padding:10}}>          
+          <View style={{justifyContent:'center', flexDirection :'row',padding:10}}>
                <View style= {styles.leftContainer}>
                   <Text style = {{fontSize:15,color:'#272822'}}>{newsItem.title}</Text>
                   <View style = {{flex:1,justifyContent:'flex-end'}} >
                     <Text >{newsItem.date}</Text>
-                  </View>                   
+                  </View>
                </View>
                <Image
                   style = {styles.thumbnail}
                   source={{uri:newsItem.custom_fields.thumb_c[0]}}/>
-          </View>          
+          </View>
           <View style={{backgroundColor:'#d8d8d8',height:0.5,flexDirection: 'row'}}/>
         </View>
       </TouchableHighlight>
-  
+
     );
   }
 
-  onRefresh(){
-      this.setState({isRefreshing: true,});
-      this.fetchNewsData();
+  onRefresh() {
+    this.setState({
+      isRefreshing: true,
+    });
+    this.fetchNewsData();
   }
 
 
-  fetchNewsData(){
+  fetchNewsData() {
     pageIndex = 1,
-    fetch(url+pageIndex)
+      fetch(url + pageIndex)
       .then((response) => response.json())
 
-      .then((responseData) => {
+    .then((responseData) => {
         this.setState({
-          newContent:responseData.posts,
-          dataSource:this.state.dataSource.cloneWithRows(responseData.posts),
+          newContent: responseData.posts,
+          dataSource: this.state.dataSource.cloneWithRows(responseData.posts),
           loaded: true,
-          isRefreshing:false
+          isRefreshing: false
         });
-        
-       AsyncStorage.setItem('NewsCache',JSON.stringify(responseData),()=>{
-       
-       //  ToastAndroid.show(,2000);
-       });
+
+        AsyncStorage.setItem('NewsCache', JSON.stringify(responseData), () => {
+
+          //  ToastAndroid.show(,2000);
+        });
         // ToastAndroid.show(JSON.stringify(responseData),2000);
 
-       
 
-        
-      }).catch((err)=>{ToastAndroid.show(err.message,3000)})
+
+      }).catch((err) => {
+        ToastAndroid.show(err.message, 3000)
+      })
       .done();
   }
 
-  refreshFromHomePage(){
-     this.setState({isRefreshing: true,});
-     fetchNewsData();
+  refreshFromHomePage() {
+    this.setState({
+      isRefreshing: true,
+    });
+    fetchNewsData();
   }
 
-  loadmore(){
-     if(this.state.loadmore) return;
-     this.setState({loadmore:true});
-     pageIndex++;
-     fetch(url+pageIndex)
+  loadmore() {
+    if (this.state.loadmore) return;
+    this.setState({
+      loadmore: true
+    });
+    pageIndex++;
+    fetch(url + pageIndex)
       .then((response) => response.json())
       .then((responseData) => {
-        
+
         this.setState({
-          newContent:[...this.state.newContent, ...responseData.posts],
-          dataSource:this.state.dataSource.cloneWithRows(this.state.newContent),
+          newContent: [...this.state.newContent, ...responseData.posts],
+          dataSource: this.state.dataSource.cloneWithRows(this.state.newContent),
           loaded: true,
-          isRefreshing:false,
-          loadmore:false
+          isRefreshing: false,
+          loadmore: false
         });
-      }).catch((err)=>{ToastAndroid.show(err.message,1000)})
+      }).catch((err) => {
+        ToastAndroid.show(err.message, 1000)
+      })
       .done();
   }
 
 
-  pressRow(id,title){
-        this.props.navigator.push({
-             title:'NewsPage',
-             name:'NewsPage',
-             params:{id:id,title:title}
-              
-            });
+  pressRow(id, title) {
+    this.props.navigator.push({
+      title: 'NewsPage',
+      name: 'NewsPage',
+      params: {
+        id: id,
+        title: title
+      }
+
+    });
   }
 
- 
 
 
 }
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent:'center',
-    flexDirection :'row', 
+    justifyContent: 'center',
+    flexDirection: 'row',
   },
-  leftContainer:{
-    height:60,
-    flexDirection :'column', 
-    flex: 1,  
+  leftContainer: {
+    height: 60,
+    flexDirection: 'column',
+    flex: 1,
     marginRight: 5,
     backgroundColor: 'white',
   },
-  thumbnail:{
-        width:90,
-        height:60,
-    },
+  thumbnail: {
+    width: 90,
+    height: 60,
+  },
 });
 
 export default NewsList;
--- TestPage.android.js
+++ TestPage.android.js
@@ -4,8 +4,8 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
+ import React, { Component } from 'react';
+ import {
   AppRegistry,
   StyleSheet,
   Text,
@@ -27,21 +27,21 @@ const  WINDOW_WIDTH = Dimensions.get('window').width;
 class TestPage extends Component {
   constructor(props) {
     super(props);
-  
+    
     this.state = {
       dataSource: new ViewPager.DataSource({pageHasChanged: (p1, p2) => p1 !== p2,})
     };
   }
 
-   componentDidMount(){
+  componentDidMount(){
     this.setState({
       dataSource:this.state.dataSource.cloneWithPages([
-  'https://images.unsplash.com/photo-1441742917377-57f78ee0e582?h=1024',
-  'https://images.unsplash.com/photo-1441716844725-09cedc13a4e7?h=1024',
-  'https://images.unsplash.com/photo-1441448770220-76743f9e6af6?h=1024',
-  'https://images.unsplash.com/photo-1441260038675-7329ab4cc264?h=1024',
-  
-])
+        'https://images.unsplash.com/photo-1441742917377-57f78ee0e582?h=1024',
+        'https://images.unsplash.com/photo-1441716844725-09cedc13a4e7?h=1024',
+        'https://images.unsplash.com/photo-1441448770220-76743f9e6af6?h=1024',
+        'https://images.unsplash.com/photo-1441260038675-7329ab4cc264?h=1024',
+        
+        ])
     });
   }
 
@@ -51,81 +51,81 @@ class TestPage extends Component {
   render() {
     return (
       <View style={{flex:3}}>
-        <View style={{height:200 , flex:1}}>
-          <Swiper style={styles.wrapper} showsButtons={false} autoplay={true} showsPagination={true} >
-              <Image style={styles.slide1} 
-              source={{uri:'http://pic.58pic.com/58pic/12/64/27/55U58PICrdX.jpg'}}
-              resizeMode={Image.resizeMode.stretch}
-              ></Image>
-             
-              <Image style={styles.slide2} 
-              source={{uri:'http://pic30.nipic.com/20130626/8174275_085522448172_2.jpg'}}
-               resizeMode={Image.resizeMode.stretch}
-              ></Image>
-             
-              <Image style={styles.slide3} 
-              source={{uri:'http://img.taodiantong.cn/v55183/infoimg/2013-07/130720115322ky.jpg'}}
-               resizeMode={Image.resizeMode.stretch}
-              ></Image>
-             
-          </Swiper>
-        </View>
-        <ViewPager
-        style={{height:200,width:WINDOW_WIDTH, flex:1}}
-        dataSource={this.state.dataSource}
-        renderPage={(data,pageID)=>this._renderPage(data,pageID)}
-        isLoop={true}
-        autoPlay={true}/>
+      <View style={{height:200 , flex:1}}>
+      <Swiper style={styles.wrapper} showsButtons={false} autoplay={true} showsPagination={true} >
+      <Image style={styles.slide1} 
+      source={{uri:'http://pic.58pic.com/58pic/12/64/27/55U58PICrdX.jpg'}}
+      resizeMode={Image.resizeMode.stretch}
+      ></Image>
+      
+      <Image style={styles.slide2} 
+      source={{uri:'http://pic30.nipic.com/20130626/8174275_085522448172_2.jpg'}}
+      resizeMode={Image.resizeMode.stretch}
+      ></Image>
+      
+      <Image style={styles.slide3} 
+      source={{uri:'http://img.taodiantong.cn/v55183/infoimg/2013-07/130720115322ky.jpg'}}
+      resizeMode={Image.resizeMode.stretch}
+      ></Image>
+      
+      </Swiper>
+      </View>
+      <ViewPager
+      style={{height:200,width:WINDOW_WIDTH, flex:1}}
+      dataSource={this.state.dataSource}
+      renderPage={(data,pageID)=>this._renderPage(data,pageID)}
+      isLoop={true}
+      autoPlay={true}/>
       <Image source={{uri:'https://images.unsplash.com/photo-1440847899694-90043f91c7f9?h=1024'}} style={{height:200,width:WINDOW_WIDTH, flex:1,}}>
       
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-           React Native background image!
-        </Text>
-        <TouchableHighlight  onPress={() => this.showTimePicker()}  underlayColor='transparent'>
-        <Text style={styles.instructions} >
-           AndroidTimePicker
-        </Text>
-        </TouchableHighlight>
-        <TouchableHighlight  onPress={() => this.showDialog()}  underlayColor='transparent'>
-          <Text style={styles.instructions}>
-            Dialog
-          </Text>
-        </TouchableHighlight>
-       </View>
-        </Image>
-       </View>
-    );
-  }
+      <Text style={styles.welcome}>
+      React Native background image!
+      </Text>
+      <TouchableHighlight  onPress={() => this.showTimePicker()}  underlayColor='transparent'>
+      <Text style={styles.instructions} >
+      AndroidTimePicker
+      </Text>
+      </TouchableHighlight>
+      <TouchableHighlight  onPress={() => this.showDialog()}  underlayColor='transparent'>
+      <Text style={styles.instructions}>
+      Dialog
+      </Text>
+      </TouchableHighlight>
+      </View>
+      </Image>
+      </View>
+      );
+}
 
-  showTimePicker(){
-    try {
-          const {action, hour, minute} =   TimePickerAndroid.open({
-            hour: 14,
-            minute: 0,
+showTimePicker(){
+  try {
+    const {action, hour, minute} =   TimePickerAndroid.open({
+      hour: 14,
+      minute: 0,
             is24Hour: false, // Will display '2 PM'
           });
-          if (action == TimePickerAndroid.timeSetAction) {
-           ToastAndroid.show(hour,1000);
-          }
-      } catch ({code, message}) {
-          console.warn('Cannot open time picker', message);
-      }
-  }
+    if (action == TimePickerAndroid.timeSetAction) {
+     ToastAndroid.show(hour,1000);
+   }
+ } catch ({code, message}) {
+  console.warn('Cannot open time picker', message);
+}
+}
 
-  showDialog(){Alert.alert('Dialog','this is dialog',[
-    {text: 'no', onPress: () => ToastAndroid.show('no pressed',1000)},
-    {text: 'yes', onPress: () => ToastAndroid.show('yes pressed',1000)}
-    ])}
+showDialog(){Alert.alert('Dialog','this is dialog',[
+  {text: 'no', onPress: () => ToastAndroid.show('no pressed',1000)},
+  {text: 'yes', onPress: () => ToastAndroid.show('yes pressed',1000)}
+  ])}
 
-   _renderPage(data,pageID){
-    return (
-      <Image
-        source={{uri: data}}
-        style={styles.slide1} />
+_renderPage(data,pageID){
+  return (
+    <Image
+    source={{uri: data}}
+    style={styles.slide1} />
     );
-  }
-  
+}
+
 }
 
 const styles = StyleSheet.create({
@@ -146,14 +146,14 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
-     color:'red'
+    color:'red'
   },
 
-   wrapper: {
+  wrapper: {
     
   },
   slide1: {
-     
+   
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#9DD6EB',
@@ -163,25 +163,25 @@ const styles = StyleSheet.create({
   },
   slide2: {
    height:200,
-    width:WINDOW_WIDTH,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#97CAE5',
-     
-  },
-  slide3: {
-    height:200,
-    width:WINDOW_WIDTH,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9',
-    
-  },
-  text: {
-    color: '#fff',
-    fontSize: 30,
-    fontWeight: 'bold',
-  }
+   width:WINDOW_WIDTH,
+   justifyContent: 'center',
+   alignItems: 'center',
+   backgroundColor: '#97CAE5',
+   
+ },
+ slide3: {
+  height:200,
+  width:WINDOW_WIDTH,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#92BBD9',
+  
+},
+text: {
+  color: '#fff',
+  fontSize: 30,
+  fontWeight: 'bold',
+}
 });
 
 export default TestPage;
